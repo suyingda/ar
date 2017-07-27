e434481e44a30f609bb46a4c357759f9f8763986
suyingda <syq1023032120@qq.com>
2017-07-27 20:46:06 +0800

    创建一个对象 （空） 存储一个Object 值即可

--- src/components/Hello1.vue
+++ src/components/Hello1.vue
@@ -1,54 +1,67 @@
 <template>
   <div id="app">
- {{arrarr}}<br/>a
-               <div v-for="(ll,index,indexd) in arrarr"  >
-               鎴戞槸index{{indexd}}<br/>
-      鎴戞槸鏍囬鍒嗙粍: {{ll[0].name}}
-                <div v-for="l in ll"  >
-               <li v-for="iii in l">{{iii}}</li>
-               </div>
-               </div>
-  </div>
+
+
+   {{arrarr}}<br/>a
+   <div v-for="(ll,index,indexd) in arrarr"  >
+     i  am  index{{indexd}}<br/>
+     i am is  title: {{ll[0].name}}
+     <div v-for="l in ll"  >
+       <li v-for="iii in l">{{iii}}</li>
+     </div>
+   </div>
+   <br/>
+   <span v-for='data2 in groupOrderData2'> {{data2}}</span>
+ </div>
+ 
 </template>
 
 <script>
 
 
-export default {
-  name: 'app',
-  data(){
+  export default {
+    name: 'app',
+    data(){
       return {
         arrarr:{},
+        groupOrderData2:{},
         arr:[
-          {"name":"suyingda","age":23},
-          {"name":"maomao","age":33},
-          {"name":"suyingda","age":13},
-          {"name":"tankang","age":63},
-          {"name":"maomao","age":43},
-          {"name":"xiaoli","age":3},
-      ]
+        {"name":"suyingda","age":23,'b':1},
+        {"name":"maomao","age":33,'b':1},
+        {"name":"suyingda","age":13,'b':22},
+        {"name":"tankang","age":63,'b':22},
+        {"name":"maomao","age":43,'b':56},
+        {"name":"xiaoli","age":3,'b':77},
+        ]
       }
     }
     ,
-      methods:{
-        abc(){
-          var  groupOrderData={};
-            this.arr.forEach((v) =>
-                {
-                    !groupOrderData[v.name] && (groupOrderData[v.name] = []);
-                    groupOrderData[v.name].push(v);
-                });
-            console.log(groupOrderData)
-           this.arrarr=groupOrderData;
-        }
-      }
-    ,mounted(){
-      this.abc();
-    }
- 
+    methods:{
+      abc(){
+        var  groupOrderData={};  //进行大分类
+var groupOrderData2={}; //再分类
+this.arr.forEach((v) =>
+{
+  !groupOrderData[v.name] && (groupOrderData[v.name] = []);
+  groupOrderData[v.name].push(v);
+
+  !groupOrderData2[v.b] && (groupOrderData2[v.b] = []);
+//  groupOrderData2[v.b].push(v);
+
+});
+this.arrarr=groupOrderData;
+console.log(groupOrderData2)
+for(var i in groupOrderData2)
+  this.groupOrderData2[i]=i
+}
+}
+,mounted(){
+  this.abc();
+}
+
 }
 </script>
 
 <style>
- 
+
 </style>
